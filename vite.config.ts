4a1037fd8bcc7814665eb4fbf2f648277538a37d
lanberb <lanberb.2@gmail.com>
2026-02-14 17:35:05 +0900

    chore: 環境整備

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from 'vite';
+import { injectHtmlsPlugin } from './plugins/injectHtmlsPlugin';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+     injectHtmlsPlugin(),
+     react({
+       jsxImportSource: "@emotion/react",
+     }),
+    ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@/components": path.resolve(__dirname, "src/components"),
+    },
+  },
+  ssr: {
+    target: "webworker",
+  }
+});
